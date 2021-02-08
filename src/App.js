e2f58fea3e41efe823987726101c92c99f6b7aa8
Jeyko Caicedo <jakeactually@outlook.com>
2021-02-07 19:47:31 -0500

    Initial

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import * as cards from './cards.js';
+
+const all = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'].flatMap(number =>
+  ['C', 'D', 'H', 'S'].map(sign =>
+    cards[`card_${number}${sign}`]
+  )
+);
 
 function App() {
+  const hand = Array(5).fill().map(() => all[Math.floor(Math.random() * all.length)]);
+  const board = Array(20).fill().map(() => all[Math.floor(Math.random() * all.length)]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div id="board">
+          {board.map(card => <img src={card} />)}
+        </div>
+        <div id="hand">
+          {hand.map(card => <img src={card} />)}
+        </div>
       </header>
     </div>
   );
---
+++ src/cards.js
@@ -0,0 +1,107 @@
+import card_10C from './cards/10C.svg';
+import card_10D from './cards/10D.svg';
+import card_10H from './cards/10H.svg';
+import card_10S from './cards/10S.svg';
+import card_2C from './cards/2C.svg';
+import card_2D from './cards/2D.svg';
+import card_2H from './cards/2H.svg';
+import card_2S from './cards/2S.svg';
+import card_3C from './cards/3C.svg';
+import card_3D from './cards/3D.svg';
+import card_3H from './cards/3H.svg';
+import card_3S from './cards/3S.svg';
+import card_4C from './cards/4C.svg';
+import card_4D from './cards/4D.svg';
+import card_4H from './cards/4H.svg';
+import card_4S from './cards/4S.svg';
+import card_5C from './cards/5C.svg';
+import card_5D from './cards/5D.svg';
+import card_5H from './cards/5H.svg';
+import card_5S from './cards/5S.svg';
+import card_6C from './cards/6C.svg';
+import card_6D from './cards/6D.svg';
+import card_6H from './cards/6H.svg';
+import card_6S from './cards/6S.svg';
+import card_7C from './cards/7C.svg';
+import card_7D from './cards/7D.svg';
+import card_7H from './cards/7H.svg';
+import card_7S from './cards/7S.svg';
+import card_8C from './cards/8C.svg';
+import card_8D from './cards/8D.svg';
+import card_8H from './cards/8H.svg';
+import card_8S from './cards/8S.svg';
+import card_9C from './cards/9C.svg';
+import card_9D from './cards/9D.svg';
+import card_9H from './cards/9H.svg';
+import card_9S from './cards/9S.svg';
+import card_AC from './cards/AC.svg';
+import card_AD from './cards/AD.svg';
+import card_AH from './cards/AH.svg';
+import card_AS from './cards/AS.svg';
+import card_JC from './cards/JC.svg';
+import card_JD from './cards/JD.svg';
+import card_JH from './cards/JH.svg';
+import card_JS from './cards/JS.svg';
+import card_KC from './cards/KC.svg';
+import card_KD from './cards/KD.svg';
+import card_KH from './cards/KH.svg';
+import card_KS from './cards/KS.svg';
+import card_QC from './cards/QC.svg';
+import card_QD from './cards/QD.svg';
+import card_QH from './cards/QH.svg';
+import card_QS from './cards/QS.svg';
+
+export {
+    card_10C,
+    card_10D,
+    card_10H,
+    card_10S,
+    card_2C,
+    card_2D,
+    card_2H,
+    card_2S,
+    card_3C,
+    card_3D,
+    card_3H,
+    card_3S,
+    card_4C,
+    card_4D,
+    card_4H,
+    card_4S,
+    card_5C,
+    card_5D,
+    card_5H,
+    card_5S,
+    card_6C,
+    card_6D,
+    card_6H,
+    card_6S,
+    card_7C,
+    card_7D,
+    card_7H,
+    card_7S,
+    card_8C,
+    card_8D,
+    card_8H,
+    card_8S,
+    card_9C,
+    card_9D,
+    card_9H,
+    card_9S,
+    card_AC,
+    card_AD,
+    card_AH,
+    card_AS,
+    card_JC,
+    card_JD,
+    card_JH,
+    card_JS,
+    card_KC,
+    card_KD,
+    card_KH,
+    card_KS,
+    card_QC,
+    card_QD,
+    card_QH,
+    card_QS,
+}
