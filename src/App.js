657adfeac10da8aca1fe1d23933af5c3b12f64c2
Jeyko Caicedo <jakeactually@outlook.com>
2021-02-13 14:32:35 -0500

    action button

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import './App.css';
 import * as cards from './cards.js';
+import { Set, OrderedSet } from 'immutable';
+import { useState } from 'react';
 
 const all = ['A', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K'].flatMap(number =>
   ['C', 'D', 'H', 'S'].map(sign =>
-    cards[`card_${number}${sign}`]
+    ({ key: `${number}${sign}`, img: cards[`card_${number}${sign}`] })
   )
 );
 
+const toggle = (set, item) => set.has(item) ? set.delete(item) : set.add(item);
+
+const actionText = (handSelection, boardSelection) => {
+  if (handSelection.size) {
+    return [...handSelection, ...boardSelection].join(' + ');
+  }
+
+  if (boardSelection.size) {
+    return 'CLAIM ' + [...boardSelection].join(', ');
+  }
+
+  return '';
+};
+
 function App() {
-  const hand = Array(5).fill().map(() => all[Math.floor(Math.random() * all.length)]);
-  const board = Array(20).fill().map(() => all[Math.floor(Math.random() * all.length)]);
+  const [hand, setHand] = useState(new OrderedSet(
+    Array(5).fill().map(() => all[Math.floor(Math.random() * all.length)])
+  ));
+  const [board, setBoard] = useState(new OrderedSet(
+    Array(20).fill().map(() => all[Math.floor(Math.random() * all.length)])
+  ));
+  const [handSelection, setHandSelection] = useState(new Set());
+  const [boardSelection, setBoardSelection] = useState(new Set());
 
   return (
     <div className="App">
       <header className="App-header">
+        <div id="action">
+          {actionText(handSelection, boardSelection)}
+        </div>
         <div id="board">
-          {board.map(card => <img src={card} />)}
+          {board.map(card =>
+            <img
+              src={card.img}
+              className={boardSelection.has(card.key) ? 'selected' : ''}
+              onClick={() => setBoardSelection(toggle(boardSelection, card.key))} />)}
         </div>
         <div id="hand">
-          {hand.map(card => <img src={card} />)}
+          {hand.map(card =>
+            <img
+            src={card.img}
+            className={handSelection.has(card.key) ? 'selected' : ''}
+            onClick={() => setHandSelection(toggle(handSelection, card.key))} />)}
         </div>
       </header>
     </div>
